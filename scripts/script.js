65c2695e80f45b6030cdfcdb1e04fa3afb69535d
Tanya Murrow <tnmurro1@HobbyLobby.corp>
2018-05-24 13:35:20 -0500

    Show entry screen and start of combat

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 
   $('#yourTurnModal').modal('show');
 
+  $('#initiativeRollModal').modal('show');
+
   $('#yourTurnModal').on('shown.bs.modal', function (event) {
     navigator.vibrate(1000);
     console.log("vibrate");
